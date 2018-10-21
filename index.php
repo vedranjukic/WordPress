889588c2ef00397235967a56b0a7ea42765e0d07
vedranjukic <vedran.jukic@gmail.com>
2018-10-21 20:06:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,5 +13,7 @@
  */
 define( 'WP_USE_THEMES', true );
 
+echo '**** EVO NEKI TEST ****'
+
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
